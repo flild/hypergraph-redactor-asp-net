a0d99ebe68df8b49baeda6b7115cf3c8869a422c
flild <35297213+flild@users.noreply.github.com>
2022-05-17 17:38:43 +0300

    -

--- Blazor_DIp/design/CustomModels.cs
+++ Blazor_DIp/design/CustomModels.cs
@@ -1,17 +1,17 @@
-/*using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Geometry;
 using Blazor.Diagrams.Core.Models;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Blazor_DIp.design
 {
-    public class Standart : NodeModel
+    public class CustomModels : NodeModel
     {
-        public Standart(Point? position = null) : base(position) { }
+        public CustomModels(Point? position = null) : base(position) { }
 
-        public string Name { get; set; }
+        public string Name10 { get; set; }
 
         // Here, you can put whatever you want, such as a method that does the addition
     }
 
-}*/
+}
